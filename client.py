6ad948528fb9aec5a4614d4c568916de7b51bd22
mingming741 <419955347@qq.com>
2020-08-09 13:40:12 +0800

    2333

--- client.py
+++ client.py
@@ -1,77 +1,70 @@
-import socket  
-import datetime 
-import time 
+#! /usr/bin/python3
+
+import socket
+import datetime
+import time
 import errno
 import sys
 
-file_name = sys.argv[1]
-print(file_name) 
-
 
 def utf8len(s):
-    return len(s.encode('utf-8'))
+    return len(s.decode('utf-8'))
 
+def main():
+    file_name = 'temp/client_recieve_temp.txt'
+    server_address = ('192.168.80.77', 7777)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_socket.settimeout(3)
 
-# the first half, mainly to get the IP address and port number from the peer
-address = ('103.49.160.139', 33333)  
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
-s.settimeout(0.1)
-msg = "to get the ip address and port of the peer"
-
-
-starting_time = time.time()
-
-sent = 0
-throughput_begin = time.time() + 1
-data = ""
-temp = 0
-
-
-
-while True:
-    fw = open(file_name, "a")
-    
-    s.sendto(msg, address)
-    print("sending request now!!!!!!!!!!")
-    fw.write("sending request now!!!!!!!!!\n")
-    begin = time.time()
+    starting_time = time.time()
     throughput_begin = time.time() + 1
+    file_write = open(file_name, "a")
 
+    sent = 0
+    data = ""
+    temp = 0
+
+    connection_index = 1
+    print("LTE connection server, start~~")
     while True:
-        try:
-            data, addr = s.recvfrom(10000000)
-        except socket.timeout:
-            print 'caught a timeout'
-            #fw.write("caught a timeout\n")
+        print("")
+        client_socket.sendto("Client Set Up connection message to server".encode(), server_address)
+
+        file_write.write("sending request now!!!!!!!!!\n")
+        begin = time.time()
+        throughput_begin = time.time() + 1
+        while True:
+            try:
+                data, addr = client_socket.recvfrom(10000000)
+            except socket.timeout:
+                print('caught a timeout')
+            now = time.time()
+            if now - begin > 30:
+                break
+                print("break")
+
+
+            if now - throughput_begin  > 1:
+                sent = sent + utf8len(data)
+                temp = sent * 8
+                print("bandwidth=  "),
+                print(temp , now - starting_time)
+                file_write.write("bandwidth: " + str(temp) + "\n")
+                sent = 0
+                throughput_begin  = throughput_begin  + 1
+            else:
+                sent = sent + utf8len(data)
+            if now - starting_time > 60:
+                break
         now = time.time()
-        if now - begin > 30:
-            break
-            print("break")
-
-
-        if now - throughput_begin  > 1:
-            sent = sent + utf8len(data)
-            #print(data)
-            temp = sent * 8 
-            print("bandwidth=  "),
-            print(temp , now - starting_time)
-            fw.write("bandwidth: " + str(temp) + "\n")
-            sent = 0
-            throughput_begin  = throughput_begin  + 1
-        else:
-            sent = sent + utf8len(data)
-            #print(data)
-
-
-
         if now - starting_time > 60:
             break
-    now = time.time()
-    if now - starting_time > 60:
-        break
+    client_socket.close()
+    file_write.close()
 
 
-        
-    fw.close()
-s.close()
-fw.close()
+
+
+
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -1,55 +1,69 @@
+#! /usr/bin/python3
+
 import socket
 import time
-address = ('103.49.160.139', 33333)
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(address)
-s.settimeout(0.01)
-addr = ()
-port = ""
-data = ""
-client_ip = ""
 
-previous_ip = ""
-previous_port = ""
-fr = open("input_file.txt")
-msg = fr.readline()
-while 1:
+
+def main():
+    server_address = ('192.168.80.77', 7777)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind(server_address)
+    previous_connection_ip = ""
+    previous_connection_port = ""
+    data = ""
+    client_address = ""
+    file_read = open("input_file.txt")
+    msg_byte = file_read.readline()[0:1000].encode()
+    file_read.close()
+    connection_total_time = 600
+    connection_log_interval = 10
+    server_socket.settimeout(5)
+
+    connection_index = 1
+    print("LTE connection server, start~~")
+    while True:
+        print("\nConnection [{}] start".format(connection_index))
         try:
-                data, addr = s.recvfrom(2048)
+            data, client_address = server_socket.recvfrom(2048)
         except socket.timeout:
-                print("caught a timeout")
+            print("Connection [{}], Client connection setup timeout".format(connection_index))
+            client_address = ""
+        if len(client_address) != 0:
+            client_ip = client_address[0]
+            client_port = client_address[1]
+            print("Connection [{}], Connection Setup success, client ip {}, client port {}".format(connection_index, client_ip, client_port))
+            if previous_connection_ip == "":
+                print("Initial first connection~~")
+            elif previous_connection_ip != client_ip or previous_connection_port != client_port:
+                print("Warning! IP and port changed: {}->{}, {}->{}".format(connection_index, previous_connection_ip, client_ip, previous_connection_port, client_port))
+            previous_connection_ip = client_ip
+            previous_connection_port = client_port
 
-        if addr !=  ():
-                client_ip = addr[0]
-                port = addr[1]
-
-        if data == "":
-                print("No client connected")
-        elif previous_ip == client_ip and previous_port == port:
-                print("IP and port do not changed:", client_ip, port)
-        else:
-                print("Warning! IP and port do changed:", client_ip, port)
-
-        previous_ip = client_ip
-        previous_port = port
-
-        msg = msg[0:1000]
-        #print(msg)
-
-
-        now = time.time()
-        count = 1
-        while 1:
+            connection_start_time = time.time()
+            while True:
                 current_time = time.time()
-                if current_time - now > 5 * count:
-                        print(current_time - now)
-                        count = count + 1
-                if addr != ():
-                        s.sendto(msg, addr)
-                        #time.sleep(8000/20000000)
-                        if (count % 10 == 0):
-                                print("sending data... counting: "),
-                                print(count)
-                if current_time - now > 30:
+                if (current_time - connection_start_time) % connection_log_interval == 0:
+                    print("Time {:.2f} ".format(current_time - connection_start_time))
+                if len(client_address) != 0:
+                    server_socket.sendto(msg_byte, client_address)
+                    if current_time - connection_start_time > connection_total_time:
                         break
-s.close()
+        else:
+            print("Connection [{}], No Client connected".format(connection_index))
+        connection_index = connection_index + 1
+    server_socket.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
