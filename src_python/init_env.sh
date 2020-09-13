c54d0767993e38e250df2b5e36f57260be928530
mingming741 <419955347@qq.com>
2020-09-13 16:43:11 +0800

    2333

--- src_python/init_env.sh
+++ src_python/init_env.sh
@@ -2,6 +2,9 @@ sudo apt-get install -y python3-pip
 sudo apt-get install -y python3-tk
 sudo apt-get install -y wireshark
 sudo apt-get install -y tshark
+sudo apt-get install -y iperf3
+git config --global user.email 419955347@qq.com
+git config --global user.name mingming741
 python3 -m pip install --upgrade pip
 python3 -m pip install matplotlib
 python3 -m pip install pandas
