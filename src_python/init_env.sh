284b4c6ffc98fc1cf3bb53662d6e5474d60dcb58
mingming741 <419955347@qq.com>
2020-09-13 16:32:27 +0800

    create init env

---
+++ src_python/init_env.sh
@@ -0,0 +1,7 @@
+sudo apt-get install -y python3-pip
+sudo apt-get install -y python3-tk
+sudo apt-get install -y wireshark
+sudo apt-get install -y tshark
+python3 -m pip install --upgrade pip
+python3 -m pip install matplotlib
+python3 -m pip install pandas
