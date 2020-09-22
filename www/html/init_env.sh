a4993890392497f6aec51407c10d05ae72c4a5fb
mingming741 <419955347@qq.com>
2020-09-22 12:10:21 +0800

    2333

--- www/html/init_env.sh
+++ www/html/init_env.sh
@@ -1,3 +1,18 @@
-sudo apt-get install python3
-sudo a2enmod cgi
-sudo service apache2 restart
+#sudo apt-get update
+#sudo apt-get install -y python3
+#sudo apt-get install -y apache2
+#sudo apt-get install -y sqlite3
+#sudo apt-get install -y sqlite
+#sudo apt-get install -y sqlitebrowser
+#sudo apt-get install -y php libapache2-mod-php
+#sudo apt-get install -y php7.0-sqlite
+
+#sudo cp -R www/ /var
+#cd /var/www
+#sudo chmod -R 777 db/
+#cd html/
+#sudo mkdir trace
+
+
+#sudo a2enmod cgi
+#sudo service apache2 restart
