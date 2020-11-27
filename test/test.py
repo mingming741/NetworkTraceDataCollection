ca3d793353f796cbc9a559320e6f8bc178f83e36
mingming741 <419955347@qq.com>
2020-11-27 09:17:50 +0800

    auto push

--- test/test.py
+++ test/test.py
@@ -21,7 +21,7 @@ def test_scheduler():
         my_scheduler = scheduler.TraceDataSchedulerClient()
 
     my_scheduler.print_attribute()
-    my_scheduler.scheduling()
+    my_scheduler.scheduling(loop=True)
 
 
 
