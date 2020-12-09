153aa0ee7a24d8a962645b8b806ab1234e84d2eb
mingming741 <419955347@qq.com>
2020-12-09 12:37:01 +0800

    auto push

--- helper/disable_tso_rso.sh
+++ helper/disable_tso_rso.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
 # Replace "ethX" with your network interface
-ethtool -K ethX tso off gso off gro off
+Active_netcard=$(ip addr show | awk '/inet.*brd/{print $NF}')
+echo $Active_netcard
+ethtool -K $Active_netcard tso off gso off gro off
