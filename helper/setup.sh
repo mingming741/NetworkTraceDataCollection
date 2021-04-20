e4d218247e9079d6636bc48f46fd25d07b50f38c
mingming741 <419955347@qq.com>
2021-04-20 16:05:19 +0800

    auto push

--- helper/setup.sh
+++ helper/setup.sh
@@ -3,13 +3,13 @@ sudo apt-get install -y python3-tk
 sudo apt-get install -y wireshark
 sudo apt-get install -y tshark
 sudo apt-get install -y iperf3
+sudo apt-get install -y python-setuptools
+sudo apt-get install -y python-pip
+sudo apt-get install -y python3-pip
 sudo apt install -y curl
+curl https://bootstrap.pypa.io/pip/3.5/get-pip.py -o get-pip.py
 git config --global user.email 419955347@qq.com
 git config --global user.name mingming741
-sudo apt-get install python-setuptools
-sudo apt-get install python-pip
-sudo apt-get install python3-pip
-curl https://bootstrap.pypa.io/pip/3.5/get-pip.py -o get-pip.py
 python3 get-pip.py
 python3 -m pip install --upgrade pip
 python3 -m pip install matplotlib
